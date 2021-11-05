0181fa09c6d042f10e99fceb0ad271b49261a7f6
filipeferreiramariz@outlook.com <filipeferreiramariz@outlook.com>
2021-11-05 11:41:43 -0300

    feat: added new dependeces

---
+++ client/js/app.js
@@ -0,0 +1,5 @@
+var agendeMais = angular.module('medServices', ['mentio', 'angularMoment', 'ngFileUpload', 'ui.calendar', 'ui.bootstrap', 'ui.mask', 'ui.router', 'ui.utils.masks', 'dndLists', 'ui.select', 'monospaced.elastic', 'rzModule', 'ngResource']);
+
+agendeMais.config(function($stateProvider) {
+
+});
